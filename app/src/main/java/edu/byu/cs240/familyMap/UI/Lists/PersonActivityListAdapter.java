a7a6564c7b3cd6f5f05f859b10a596f9f9683928
JacobAmosa <jacobamosa@gmail.com>
2021-12-08 23:28:32 -0700

    finsihed working on adapter

--- app/src/main/java/edu/byu/cs240/familyMap/UI/Lists/PersonActivityListAdapter.java
+++ app/src/main/java/edu/byu/cs240/familyMap/UI/Lists/PersonActivityListAdapter.java
@@ -1,5 +1,6 @@
 package edu.byu.cs240.familyMap.UI.Lists;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,88 +16,56 @@ import edu.byu.cs240.familyMap.R;
 import shared.EventModel;
 import shared.PersonModel;
 
-/** PersonActivityListAdapter
- * This class contains all information used by the expandable list in the Person Activity
- */
 public class PersonActivityListAdapter extends BaseExpandableListAdapter {
+    private final DataCache dataCache = DataCache.getInstance();
+    private final Context myContext;
+    private final List<String> headers;
+    private final List<EventModel> events;
+    private final List<PersonModel> persons;
+    private final PersonModel myPerson;
+    private TextView lineOne;
+    private TextView lineTwo;
 
-    private Context context;
-    private List<String> mHeaders;
-    private List<EventModel> mEvents;
-    private List<PersonModel> mPersons;
-    private PersonModel mCurrPerson;
-
-    private TextView mFirstLine;
-    private TextView mSecondLine;
-    private ImageView mListIcon;
-
-    private DataCache dataCache = DataCache.getInstance();
-
-    // ========================== Constructor ========================================
-    public PersonActivityListAdapter(Context context, List<String> listDataHeader,
-                                     List<EventModel> eventsList, List<PersonModel> personsList,
+    public PersonActivityListAdapter(Context myContext, List<String> data, List<EventModel> events, List<PersonModel> people,
                                      PersonModel person) {
-        this.context = context;
-        this.mHeaders = listDataHeader;
-        this.mEvents = eventsList;
-        this.mPersons = personsList;
-        this.mCurrPerson = person;
+        this.myContext = myContext;
+        this.headers = data;
+        this.events = events;
+        this.persons = people;
+        this.myPerson = person;
     }
 
-    //_______________________________ List Adapter Override Functions __________________________________________
+    @Override
+    public Object getGroup(int num) {
+        if (num == 0){
+            return events;
+        }else{
+            return persons;
+        }
+    }
+
+    @Override
+    public int getChildrenCount(int num){
+        if (num == 0){
+            return events.size();
+        }else {
+            return persons.size();
+        }
+    }
+
+    @Override
+    public Object getChild(int numOne, int numTwo) {
+        if(numOne == 0){
+            return events.get(numTwo);
+        }else{
+            return persons.get(numTwo);
+        }
+    }
+
     @Override
     public int getGroupCount()
     {
-        return mHeaders.size();
-    }
-
-    //--****************-- Get Number of Children --***************--
-    @Override
-    public int getChildrenCount(int groupPosition)
-    {
-        if (groupPosition == 0){
-            return mEvents.size();
-        }
-        else{
-            return mPersons.size();
-        }
-    }
-
-    //--****************-- Get Drop Down Group --***************--
-    @Override
-    public Object getGroup(int groupPosition)
-    {
-        if (groupPosition == 0){
-            return mEvents;
-        }
-        else{
-            return mPersons;
-        }
-    }
-
-    //--****************-- Get Child --***************--
-    @Override
-    public Object getChild(int groupPosition, int childPosition)
-    {
-        if(groupPosition == 0){
-            return mEvents.get(childPosition);
-        }
-        else{
-            return mPersons.get(childPosition);
-        }
-    }
-
-    //--****************-- Functions not used, but needed to be Overridden --***************--
-    @Override
-    public long getGroupId(int groupPosition)
-    {
-        return groupPosition;
-    }
-
-    @Override
-    public long getChildId(int groupPosition, int childPosition)
-    {
-        return childPosition;
+        return headers.size();
     }
 
     @Override
@@ -105,109 +74,109 @@ public class PersonActivityListAdapter extends BaseExpandableListAdapter {
         return false;
     }
 
-    //--****************-- Get the Header layout and inflate --***************--
     @Override
-    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent)
+    public long getGroupId(int groupPosition) {return groupPosition; }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition)
     {
-        String headerTitle = mHeaders.get(groupPosition);
-
-        if (convertView == null) {
-            LayoutInflater inflater = (LayoutInflater) this.context
-                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inflater.inflate(R.layout.list_header_event, null);
-        }
-
-        TextView header = convertView.findViewById(R.id.event_header);
-        header.setText(headerTitle);
-
-        return convertView;
+        return childPosition;
     }
 
-    //--****************-- Get the Child Layout and inflate --***************--
+    @SuppressLint("InflateParams")
     @Override
-    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent)
-    {
-        if (convertView == null) {
-            LayoutInflater inflater = (LayoutInflater) context
+    public View getGroupView(int num, boolean stretched, View convert, ViewGroup higher) {
+        String title = headers.get(num);
+        if (convert == null) {
+            LayoutInflater myInflate = (LayoutInflater) this.myContext
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inflater.inflate(R.layout.list_item_event, null);
+            convert = myInflate.inflate(R.layout.list_header_event, null);
         }
-
-        mFirstLine = convertView.findViewById(R.id.event_list_info);
-        mSecondLine = convertView.findViewById(R.id.event_list_person);
-        mListIcon = convertView.findViewById(R.id.list_item_icon);
-
-        if (groupPosition == 0) {
-            EventModel currEvent = (EventModel) getChild(groupPosition, childPosition);
-
-            mListIcon.setImageDrawable(convertView.getResources().getDrawable(R.drawable.map_pointer_icon));
-            update(currEvent, null);
-
-        }
-        else{
-            PersonModel currPerson = (PersonModel) getChild(groupPosition, childPosition);
-
-            if (currPerson.getGender().toLowerCase().equals("m")){
-                mListIcon.setImageDrawable(convertView.getResources().getDrawable(R.drawable.boy_logo));
-            }
-            else {
-                mListIcon.setImageDrawable(convertView.getResources().getDrawable(R.drawable.girl_logo));
-            }
-
-            update(null, currPerson);
-        }
-        return convertView;
+        TextView myHead = convert.findViewById(R.id.event_header);
+        myHead.setText(title);
+        return convert;
     }
 
-    //--****************-- Set Child to Selectable --***************--
+    @SuppressLint({"InflateParams", "UseCompatLoadingForDrawables"})
     @Override
-    public boolean isChildSelectable(int groupPosition, int childPosition)
-    {
+    public View getChildView(int groupNum, int kidNum, boolean lastKid, View convert, ViewGroup parent) {
+        if (convert == null) {
+            LayoutInflater myInflate = (LayoutInflater) myContext
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convert = myInflate.inflate(R.layout.list_item_event, null);
+        }
+        lineOne = convert.findViewById(R.id.event_list_info);
+        lineTwo = convert.findViewById(R.id.event_list_person);
+        ImageView icon = convert.findViewById(R.id.list_item_icon);
+        if (groupNum == 0) {
+            childHelperOne(groupNum, kidNum, icon, convert);
+        }else{
+            childHelperTwo(groupNum, kidNum, icon, convert);
+        }
+        return convert;
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    public void childHelperOne(int groupNum, int kidNum, ImageView icon, View convert){
+        EventModel event = (EventModel) getChild(groupNum, kidNum);
+        icon.setImageDrawable(convert.getResources().getDrawable(R.drawable.map_pointer_icon));
+        adjustData(event, null);
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    public void childHelperTwo(int groupNum, int kidNum, ImageView icon, View convert){
+        PersonModel person = (PersonModel) getChild(groupNum, kidNum);
+        if (person.getGender().equalsIgnoreCase("m")){
+            icon.setImageDrawable(convert.getResources().getDrawable(R.drawable.boy_logo));
+        }else {
+            icon.setImageDrawable(convert.getResources().getDrawable(R.drawable.girl_logo));
+        }
+        adjustData(null, person);
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupNum, int childNum){
         return true;
     }
 
-
-    //--****************-- Initiate Text and Icon of a Child Layout --***************--
-    private void update(EventModel events, PersonModel persons)
-    {
-        if (persons == null) {
-            String eventInfo = events.getEventType() + ", " + events.getCity() + ", " + events.getCountry() + " " + events.getYear();
-            mFirstLine.setText(eventInfo);
-            PersonModel currPerson = dataCache.getMyPeople().get(events.getPersonID());
-            String personInfo = currPerson.getFirstName() + " " + currPerson.getLastName();
-            mSecondLine.setText(personInfo);
-        }
-        else {
-            String personInfo = persons.getFirstName() + " " + persons.getLastName();
-            mFirstLine.setText(personInfo);
-            mSecondLine.setText(getRelationship(persons));
-
-        }
-    }
-
-    //--****************-- Find Relationships of a Person --***************--
-    private String getRelationship(PersonModel persons)
-    {
-        if (mCurrPerson.getSpouseID().equals(persons.getId())) {
+    private String tiesFinder(PersonModel person){
+        if (myPerson.getSpouseID().equals(person.getId())) {
             return "Spouse";
         }
-
-        if (persons.getFatherID() != null && persons.getMotherID() != null) {
-            if (persons.getFatherID().equals(mCurrPerson.getId()) ||
-                    persons.getMotherID().equals(mCurrPerson.getId())) {
+        if (person.getMotherID() != null && person.getFatherID() != null) {
+            if (person.getFatherID().equals(myPerson.getId()) ||
+                    person.getMotherID().equals(myPerson.getId())) {
                 return "Child";
-            }
-        }
-
-        if (mCurrPerson.getMotherID() != null && mCurrPerson.getMotherID() != null) {
-            if (mCurrPerson.getFatherID().equals(persons.getId())) {
+            }else if (myPerson.getFatherID().equals(person.getId())) {
                 return "Father";
-            }
-            else if (mCurrPerson.getMotherID().equals(persons.getId())) {
+            }else if (myPerson.getMotherID().equals(person.getId())) {
                 return "Mother";
             }
         }
         return "Error";
+    }
+
+    private void adjustData(EventModel myEvent, PersonModel person){
+        if (person == null) {
+            adjustEventHelper(myEvent);
+        }else{
+            adjustPersonHelper(person);
+        }
+    }
+
+    public void adjustEventHelper(EventModel myEvent){
+        PersonModel myPerson = dataCache.getMyPeople().get(myEvent.getPersonID());
+        String data = myEvent.getEventType() + ", " + myEvent.getCity() + ", " + myEvent.getCountry() + " " + myEvent.getYear();
+        assert myPerson != null;
+        String pData = myPerson.getFirstName() + " " + myPerson.getLastName();
+        lineTwo.setText(pData);
+        lineOne.setText(data);
+    }
+
+    public void adjustPersonHelper(PersonModel person){
+        String data = person.getFirstName() + " " + person.getLastName();
+        lineTwo.setText(tiesFinder(person));
+        lineOne.setText(data);
     }
 
 }
